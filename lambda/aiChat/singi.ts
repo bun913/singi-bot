470a5a77091c81fc0a62bf97f3e2b6eb40ded988
bun913 <nagasima.romance+@gmail.com>
2024-08-01 22:33:22 +0900

    feat: add sonnet setting

--- lambda/aiChat/singi.ts
+++ lambda/aiChat/singi.ts
@@ -15,8 +15,9 @@ export const singi = async (
   client: BedrockRuntime,
   messages: Message[]
 ): Promise<string> => {
+  console.log(JSON.stringify(messages))
   const res = await client.invokeModel({
-    modelId: "anthropic.claude-3-haiku-20240307-v1:0",
+    modelId: "anthropic.claude-3-sonnet-20240229-v1:0",
     body: JSON.stringify({
       anthropic_version: "bedrock-2023-05-31",
       temperature: 0.5,
